bc10e4181db53da38822c33d27596872ccfc0b33
Polokhach <polokhach_d@yahoo.com>
2015-12-07 00:47:26 +0200

    Revert "hell"
    This reverts commit 3f2b2ecb7bbd1a7a3db03bc222b3e7ffeb84e7a4.

--- BranchAndBound/Program.cs
+++ BranchAndBound/Program.cs
@@ -11,7 +11,6 @@ namespace BranchAndBound
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hELLO");
             Stopwatch sw = new Stopwatch();
             sw.Start();
             Fraction[,] a = { { 7, 4, 2}, 
@@ -19,7 +18,7 @@ namespace BranchAndBound
                 { 1, 4, 5} };
             Fraction[] b = { 9, 15, 6 };
             Fraction[] c = { 2, 15, 2};
-            SimplexTable st = new SimplexTable(a, b, c, new string[] { "<=", "<=", "<=" }); 
+            SimplexTable st = new SimplexTable(a, b, c, new string[] { "<=", "<=", "<=" });
 
             //double[,] a = { { -5, -6, 1, 0, 0 }, { -15, 0, 0, 1, 0 }, { -7, -12, 0, 0, 1} };
             //double[] b = { -1, -1, -1 };
